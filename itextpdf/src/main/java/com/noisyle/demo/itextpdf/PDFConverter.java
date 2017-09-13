be29d94acc2826f2bab9935f65fac26641fce3dd
York Wang <anyone.1984@gmail.com>
2017-09-13 17:14:15 +0800

    add Header/Footer

--- itextpdf/src/main/java/com/noisyle/demo/itextpdf/PDFConverter.java
+++ itextpdf/src/main/java/com/noisyle/demo/itextpdf/PDFConverter.java
@@ -7,11 +7,14 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.xhtmlrenderer.pdf.ITextRenderer;
@@ -46,7 +49,7 @@ public class PDFConverter {
 		template.process(param, writer);
 	}
 
-	private static void convertHtml2PDF(String html, OutputStream out) throws DocumentException, IOException {
+	private static void convertHTML2PDF(String html, OutputStream out) throws DocumentException, IOException {
 		ITextRenderer renderer = new ITextRenderer();
 		renderer.getFontResolver().addFont(PDFConverter.class.getResource("/fonts/ARIALUNI.TTF").getPath(),
 				BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
@@ -54,17 +57,32 @@ public class PDFConverter {
 		renderer.layout();
 		renderer.createPDF(out);
 	}
-
+	
 	private static List<Map<String, Object>> getFlatTitle(List<List<Map<String, Object>>> titles) {
 		// TODO compatible with multi-level headers
 		return titles != null && titles.size() > 0 ? titles.get(titles.size() - 1) : null;
 	}
+	
+	private static List<TableData> pagination(TableData tableData, int pageSize) {
+		List<TableData> tables = new LinkedList<TableData>();
+		TableData tmp = null;
+		for (int i = 0; i * pageSize < tableData.getRows().size(); i++) {
+			tmp = new TableData();
+			tmp.setTotal(tableData.getTotal());
+			tmp.setRows(tableData.getRows().subList(i * pageSize,
+					(i + 1) * pageSize < tableData.getRows().size() ? (i + 1) * pageSize : tableData.getRows().size()));
+			tmp.setFooter(tableData.getFooter());
+			tables.add(tmp);
+		}
+		return tables;
+	}
 
 	@SuppressWarnings("unchecked")
 	public static void main(String[] args) throws IOException {
+		int pageSize = 10;
 		String _table_name = "表格名称";
 		String _title = "[[{\"field\":\"userId\",\"sortable\":true,\"width\":\"100\",\"title\":\"用户ID\",\"boxWidth\":92,\"cellClass\":\"datagrid-cell-c1-userId\",\"cellSelector\":\"div.datagrid-cell-c1-userId\"},{\"field\":\"userName\",\"sortable\":true,\"width\":\"300\",\"title\":\"用户名称\",\"boxWidth\":292,\"cellClass\":\"datagrid-cell-c1-userName\",\"cellSelector\":\"div.datagrid-cell-c1-userName\"},{\"field\":\"expiryDate\",\"sortable\":true,\"width\":\"200\",\"title\":\"格式化时间\",\"boxWidth\":192,\"cellClass\":\"datagrid-cell-c1-expiryDate\",\"cellSelector\":\"div.datagrid-cell-c1-expiryDate\"},{\"field\":\"balance\",\"sortable\":true,\"width\":\"200\",\"title\":\"格式化金额\",\"boxWidth\":192,\"cellClass\":\"datagrid-cell-c1-balance\",\"cellSelector\":\"div.datagrid-cell-c1-balance\"}]]";
-		String _data = "{\"rows\":[{\"balance\":\"900,000.00\",\"ROWNUM1\":1,\"expiryDate\":\"2015-02-16\",\"userId\":74,\"userName\":\"MGMT_VIEW\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":2,\"expiryDate\":\"2015-02-16\",\"userId\":0,\"userName\":\"SYS\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":3,\"expiryDate\":\"2015-02-16\",\"userId\":5,\"userName\":\"SYSTEM\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":4,\"expiryDate\":\"2015-02-16\",\"userId\":30,\"userName\":\"DBSNMP\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":5,\"expiryDate\":\"2015-02-16\",\"userId\":72,\"userName\":\"SYSMAN\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":6,\"expiryDate\":\"2015-02-16\",\"userId\":9,\"userName\":\"OUTLN\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":7,\"expiryDate\":\"2015-02-16\",\"userId\":75,\"userName\":\"FLOWS_FILES\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":8,\"expiryDate\":\"2015-02-16\",\"userId\":57,\"userName\":\"MDSYS\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":9,\"expiryDate\":\"2015-02-16\",\"userId\":53,\"userName\":\"ORDSYS\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":10,\"expiryDate\":\"2015-02-16\",\"userId\":42,\"userName\":\"EXFSYS\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":11,\"expiryDate\":\"2015-02-16\",\"userId\":32,\"userName\":\"WMSYS\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":12,\"expiryDate\":\"2015-02-16\",\"userId\":31,\"userName\":\"APPQOSSYS\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":13,\"expiryDate\":\"2015-02-16\",\"userId\":78,\"userName\":\"APEX_030200\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":14,\"expiryDate\":\"2015-02-16\",\"userId\":83,\"userName\":\"OWBSYS_AUDIT\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":15,\"expiryDate\":\"2015-02-16\",\"userId\":54,\"userName\":\"ORDDATA\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":16,\"expiryDate\":\"2015-02-16\",\"userId\":43,\"userName\":\"CTXSYS\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":17,\"expiryDate\":\"2015-02-16\",\"userId\":46,\"userName\":\"ANONYMOUS\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":18,\"expiryDate\":\"2015-02-16\",\"userId\":45,\"userName\":\"XDB\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":19,\"expiryDate\":\"2015-02-16\",\"userId\":55,\"userName\":\"ORDPLUGINS\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":20,\"expiryDate\":\"2015-02-16\",\"userId\":79,\"userName\":\"OWBSYS\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":21,\"expiryDate\":\"2015-02-16\",\"userId\":56,\"userName\":\"SI_INFORMTN_SCHEMA\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":22,\"expiryDate\":\"2015-02-16\",\"userId\":61,\"userName\":\"OLAPSYS\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":23,\"expiryDate\":\"2015-02-16\",\"userId\":84,\"userName\":\"SCOTT\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":24,\"expiryDate\":\"2015-02-16\",\"userId\":21,\"userName\":\"ORACLE_OCM\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":25,\"expiryDate\":\"2015-02-16\",\"userId\":2147483638,\"userName\":\"XS$NULL\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":26,\"expiryDate\":\"2015-02-16\",\"userId\":90,\"userName\":\"BI\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":27,\"expiryDate\":\"2015-02-16\",\"userId\":89,\"userName\":\"PM\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":28,\"expiryDate\":\"2015-02-16\",\"userId\":65,\"userName\":\"MDDATA\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":29,\"expiryDate\":\"2015-02-16\",\"userId\":87,\"userName\":\"IX\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":30,\"expiryDate\":\"2015-02-16\",\"userId\":88,\"userName\":\"SH\"}],\"total\":30}";
+		String _data = "{\"rows\":[{\"balance\":\"900,000.00\",\"ROWNUM1\":1,\"expiryDate\":\"2015-02-16\",\"userId\":74,\"userName\":\"MGMT_VIEW\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":2,\"expiryDate\":\"2015-02-16\",\"userId\":0,\"userName\":\"SYS\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":3,\"expiryDate\":\"2015-02-16\",\"userId\":5,\"userName\":\"SYSTEM\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":4,\"expiryDate\":\"2015-02-16\",\"userId\":30,\"userName\":\"DBSNMP\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":5,\"expiryDate\":\"2015-02-16\",\"userId\":72,\"userName\":\"SYSMAN\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":6,\"expiryDate\":\"2015-02-16\",\"userId\":9,\"userName\":\"OUTLN\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":7,\"expiryDate\":\"2015-02-16\",\"userId\":75,\"userName\":\"FLOWS_FILES\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":8,\"expiryDate\":\"2015-02-16\",\"userId\":57,\"userName\":\"MDSYS\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":9,\"expiryDate\":\"2015-02-16\",\"userId\":53,\"userName\":\"ORDSYS\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":10,\"expiryDate\":\"2015-02-16\",\"userId\":42,\"userName\":\"EXFSYS\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":11,\"expiryDate\":\"2015-02-16\",\"userId\":32,\"userName\":\"WMSYS\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":12,\"expiryDate\":\"2015-02-16\",\"userId\":31,\"userName\":\"APPQOSSYS\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":13,\"expiryDate\":\"2015-02-16\",\"userId\":78,\"userName\":\"APEX_030200\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":14,\"expiryDate\":\"2015-02-16\",\"userId\":83,\"userName\":\"OWBSYS_AUDIT\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":15,\"expiryDate\":\"2015-02-16\",\"userId\":54,\"userName\":\"ORDDATA\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":16,\"expiryDate\":\"2015-02-16\",\"userId\":43,\"userName\":\"CTXSYS\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":17,\"expiryDate\":\"2015-02-16\",\"userId\":46,\"userName\":\"ANONYMOUS\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":18,\"expiryDate\":\"2015-02-16\",\"userId\":45,\"userName\":\"XDB\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":19,\"expiryDate\":\"2015-02-16\",\"userId\":55,\"userName\":\"ORDPLUGINS\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":20,\"expiryDate\":\"2015-02-16\",\"userId\":79,\"userName\":\"OWBSYS\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":21,\"expiryDate\":\"2015-02-16\",\"userId\":56,\"userName\":\"SI_INFORMTN_SCHEMA\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":22,\"expiryDate\":\"2015-02-16\",\"userId\":61,\"userName\":\"OLAPSYS\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":23,\"expiryDate\":\"2015-02-16\",\"userId\":84,\"userName\":\"SCOTT\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":24,\"expiryDate\":\"2015-02-16\",\"userId\":21,\"userName\":\"ORACLE_OCM\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":26,\"expiryDate\":\"2015-02-16\",\"userId\":90,\"userName\":\"BI\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":27,\"expiryDate\":\"2015-02-16\",\"userId\":89,\"userName\":\"PM\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":28,\"expiryDate\":\"2015-02-16\",\"userId\":65,\"userName\":\"MDDATA\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":29,\"expiryDate\":\"2015-02-16\",\"userId\":87,\"userName\":\"IX\"},{\"balance\":\"900,000.00\",\"ROWNUM1\":30,\"expiryDate\":\"2015-02-16\",\"userId\":88,\"userName\":\"SH\"}],\"total\":29}";
 
 		ObjectMapper mapper = new ObjectMapper();
 		TableData data = mapper.readValue(_data, TableData.class);
@@ -78,9 +96,7 @@ public class PDFConverter {
 		param.put("print_time", printtime);
 		param.put("title", titles);
 		param.put("flat_title", getFlatTitle(titles));
-		param.put("total", data.getTotal());
-		param.put("rows", data.getRows());
-		param.put("footer", data.getFooter());
+		param.put("table_datas", pagination(data, pageSize));
 		ByteArrayOutputStream out_template = new ByteArrayOutputStream();
 		try {
 			renderHTML("table.ftl", param, out_template);
@@ -94,9 +110,12 @@ public class PDFConverter {
 		String html = out_template.toString();
 		logger.debug(html);
 
-		OutputStream out_html = new FileOutputStream("d:\\test" + printtime.getTime() + ".pdf");
+		OutputStream out_html = new FileOutputStream("d:\\test" + printtime.getTime() + ".html");
+		IOUtils.write(html, out_html, StandardCharsets.UTF_8);
+		
+		OutputStream out_pdf = new FileOutputStream("d:\\test" + printtime.getTime() + ".pdf");
 		try {
-			convertHtml2PDF(html, out_html);
+			convertHTML2PDF(html, out_pdf);
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
@@ -104,40 +123,5 @@ public class PDFConverter {
 				out_html.close();
 			}
 		}
-	}
-}
-
-class TableData {
-	private int total;
-	private List<Map<String, Object>> rows;
-	private List<Map<String, Object>> footer;
-
-	public int getTotal() {
-		return total;
-	}
-
-	public void setTotal(int total) {
-		this.total = total;
-	}
-
-	public List<Map<String, Object>> getRows() {
-		return rows;
-	}
-
-	public void setRows(List<Map<String, Object>> rows) {
-		this.rows = rows;
-	}
-
-	public List<Map<String, Object>> getFooter() {
-		return footer;
-	}
-
-	public void setFooter(List<Map<String, Object>> footer) {
-		this.footer = footer;
-	}
-
-	@Override
-	public String toString() {
-		return String.format("total:%d rows:%s", total, rows.size());
 	}
 }
--- itextpdf/src/main/resources/ftl/table.ftl
+++ itextpdf/src/main/resources/ftl/table.ftl
@@ -3,65 +3,88 @@
 <head>
 <style>
 @page {
-	size: 11.69in 8.27in;
-	margin: 0.7in 0.4in 0.7in 0.4in;
+  size: 11.69in 8.27in;
+  margin: 0.7in 0.4in 0.7in 0.4in;
+  @top-center {
+    content: element(header)
+  }
+  @bottom-center {
+    content: element(footer)
+  }
 }
 * {
-	font-family: 'Arial Unicode MS';
-	font-size: 12px;
+  font-family: 'Arial Unicode MS';
+  font-size: 12px;
+}
+div.header {
+  display: block; text-align: center; 
+  position: running(header);
+}
+div.footer {
+  display: block; text-align: center;
+  position: running(footer);
+}
+div.content {
+  page-break-after: always;
 }
 html, body {
-	padding: 0px;
-	margin: 0px;
+  padding: 0px;
+  margin: 0px;
 }
 table {
-	width: 100%;
-	border: 0;
-	border-collapse: collapse;
-	table-layout: fixed;
-	word-break: break-strict;
-	page-break-after: always;
+  width: 100%;
+  border: 0;
+  border-collapse: collapse;
 }
 td, th {
-	border: 1px solid #000;
-	padding: 10px;
-	word-wrap: break-word;
-	overflow: hidden;
+  border: 1px solid #000;
+  padding: 10px;
+  word-wrap: break-word;
+  overflow: hidden;
 }
 th {
-	background-color: #F5F5F5;
-	text-align:center;
+  background-color: #F5F5F5;
+  text-align:center;
 }
 </style>
 </head>
 <body>
-	<table cellspacing="0" cellpadding="0">
-		<thead>
-			<tr>
-				<th colspan="${flat_title?size}">${table_name}</th>
-			</tr>
-			<#list title as row>
-			<tr>
-				<#list row as col>
-				<th>${col.title}</th>
-				</#list>
-			</tr>
-			</#list>
-		</thead>
-		<tbody>
-		<#list rows as row>
-			<tr>
-				<#list flat_title as col>
-				<td>${row[col.field]}</td>
-				</#list>
-			</tr>
-		</#list>
-		</tbody>
-		<tfoot>
-			<tr>
-				<td colspan="${flat_title?size}">打印时间: ${print_time?string("yyyy-MM-dd HH:mm:ss")}，共${total}条记录。</td>
-			</tr>
-		</tfoot>
-	</table>
+<div class='header'><h1>${table_name}</h1></div>
+<#list table_datas as table_data>
+<div class='content'>
+  <table cellspacing="0" cellpadding="0">
+    <thead>
+      <#list title as row>
+      <tr>
+        <#list row as col>
+        <th>${col.title}</th>
+        </#list>
+      </tr>
+      </#list>
+    </thead>
+    <tbody>
+    <#list table_data.rows as row>
+      <tr>
+        <#list flat_title as col>
+        <td>${row[col.field]}</td>
+        </#list>
+      </tr>
+    </#list>
+    </tbody>
+    <#if table_data.footer??>
+    <tfoot>
+    <#list table_data.footer as row>
+      <tr>
+        <#list flat_title as col>
+        <td>${row[col.field]}</td>
+        </#list>
+      </tr>
+    </#list>
+    </tfoot>
+    </#if>
+  </table>
+</div>
+</#list>
+<div class='footer'><h2>打印时间: ${print_time?string("yyyy-MM-dd HH:mm:ss")}。</h2></div>
 </body>
 </html>
