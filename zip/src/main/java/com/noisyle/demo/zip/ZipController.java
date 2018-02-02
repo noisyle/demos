6b1352ee986882d443607fd039e3cc5c7be69159
York Wang <anyone.1984@gmail.com>
2018-02-02 16:57:36 +0800

    add zip download demo

---
+++ zip/src/main/java/com/noisyle/demo/zip/ZipController.java
@@ -0,0 +1,57 @@
+package com.noisyle.demo.zip;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+@Controller
+public class ZipController {
+
+    @RequestMapping("/download")
+    public void download(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            byte[] zip = zipFiles();
+            ServletOutputStream sos = response.getOutputStream();
+            response.setContentType("application/zip");
+            response.setHeader("Content-Disposition", "attachment; filename=aaa.zip");
+            sos.write(zip);
+            sos.flush();
+        } catch (Exception e) {
+
+        }
+
+    }
+
+    private byte[] zipFiles() throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ZipOutputStream zos = new ZipOutputStream(baos);
+        byte bytes[] = new byte[2048];
+
+        for (int i = 1; i < 3; i++) {
+            Resource resource = new ClassPathResource("file" + i + ".txt");
+            BufferedInputStream bis = new BufferedInputStream(resource.getInputStream());
+            zos.putNextEntry(new ZipEntry("file" + i + ".txt"));
+            int bytesRead;
+            while ((bytesRead = bis.read(bytes)) != -1) {
+                zos.write(bytes, 0, bytesRead);
+            }
+            zos.closeEntry();
+            bis.close();
+        }
+        zos.flush();
+        baos.flush();
+        zos.close();
+        baos.close();
+
+        return baos.toByteArray();
+    }
+}
