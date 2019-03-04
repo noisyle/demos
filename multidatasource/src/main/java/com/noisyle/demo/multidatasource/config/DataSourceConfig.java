ca334530ed7c5fa50efea39f905eb00bd21552d4
York Wang <anyone.1984@gmail.com>
2019-03-04 16:21:26 +0800

    add multi-datasource demo

--- multidatasource/src/main/java/com/noisyle/demo/multidatasource/config/DataSourceConfig.java
+++ multidatasource/src/main/java/com/noisyle/demo/multidatasource/config/DataSourceConfig.java
@@ -9,7 +9,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 import com.zaxxer.hikari.HikariDataSource;
 
@@ -17,7 +16,7 @@ import com.zaxxer.hikari.HikariDataSource;
 @MapperScan(basePackages = "com.noisyle.demo.multidatasource.repository", sqlSessionFactoryRef = DataSourceConfig.PREFIX + "SqlSessionFactory")
 public class DataSourceConfig {
     
-    final static public String PREFIX = "db2";
+    final static public String PREFIX = "db1";
     
     @Value("${mybatis.mapper-locations}")
     private String mapperLocations;
@@ -30,10 +29,10 @@ public class DataSourceConfig {
     }
 
     @Bean(PREFIX + "SqlSessionFactory")
+    @ConfigurationProperties(prefix = "mybatis")
     @Primary
     public SqlSessionFactoryBean sqlSessionFactory() throws Exception {
         SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-        sqlSessionFactoryBean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources(mapperLocations));
         sqlSessionFactoryBean.setDataSource(dataSource());
         return sqlSessionFactoryBean;
     }
