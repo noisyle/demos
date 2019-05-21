4f2bbcebd73fe6dedd28e1a8dd2d4c811aeccdbf
York Wang <anyone.1984@gmail.com>
2019-05-21 15:43:21 +0800

    fix slf4j dependency

--- websocket/src/main/java/com/noisyle/demo/websocket/WebSocketController.java
+++ websocket/src/main/java/com/noisyle/demo/websocket/WebSocketController.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -17,6 +19,7 @@ import com.noisyle.demo.websocket.vo.HelloMessage;
 
 @Controller
 public class WebSocketController {
+    private Logger logger = LoggerFactory.getLogger(getClass());
 
     @Autowired
     private SimpMessagingTemplate template;
@@ -31,6 +34,7 @@ public class WebSocketController {
     public void updateLineChart() {
         Map<String, Object> res = new HashMap<String, Object>();
         res.put("data", new Random().nextInt(500) + 800);
+        logger.debug("推送消息： {}", res);
         template.convertAndSend("/topic/linechart", res);
     }
 }
