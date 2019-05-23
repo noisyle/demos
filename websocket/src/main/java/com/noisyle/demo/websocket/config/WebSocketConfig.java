bd26fae1a3123a100ece7f24169f78a61244df19
York Wang <anyone.1984@gmail.com>
2019-05-23 14:31:14 +0800

    add heartbeat setting

--- websocket/src/main/java/com/noisyle/demo/websocket/config/WebSocketConfig.java
+++ websocket/src/main/java/com/noisyle/demo/websocket/config/WebSocketConfig.java
@@ -2,6 +2,7 @@ package com.noisyle.demo.websocket.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -12,7 +13,13 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
+        // 仅当客户端不使用SockJS时(如微信小程序)，添加setTaskScheduler和setHeartbeatValue设置，由stomp协商heartbeat。
+        // 否则可以不设置，由SockJS自行协商heartbeat。
+        ThreadPoolTaskScheduler ts = new ThreadPoolTaskScheduler();
+        ts.setThreadNamePrefix("wss-heartbeat-thread-");
+        ts.initialize();
+
+        config.enableSimpleBroker("/topic").setTaskScheduler(ts).setHeartbeatValue(new long[] { 10000, 10000 });
         config.setApplicationDestinationPrefixes("/app");
     }
 
